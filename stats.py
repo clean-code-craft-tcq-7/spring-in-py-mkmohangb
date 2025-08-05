18e15d7b641a835bad62f2703a776d8b49edad46
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-05 16:45:17 +0530

    add tests for error scenarios

--- stats.py
+++ stats.py
@@ -1,8 +1,14 @@
+import math
 
 def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
   stats = {}
-  stats['avg'] = sum(numbers)/len(numbers)
-  stats['max'] = max(numbers)
-  stats['min'] = min(numbers)
+  numbers_without_nan = [num for num in numbers if not math.isnan(num)]
+  if numbers_without_nan:
+    stats['avg'] = sum(numbers_without_nan)/len(numbers_without_nan)
+    stats['max'] = max(numbers_without_nan)
+    stats['min'] = min(numbers_without_nan)
+  else:
+    stats['avg'] = float('nan')
+    stats['max'] = float('nan')
+    stats['min'] = float('nan')
   return stats
